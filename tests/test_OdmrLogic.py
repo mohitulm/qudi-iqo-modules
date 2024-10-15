3f0921f027e3dc3e7d4aade810d9ae43c6c78499
Mohit <mohit.shah@uni-ulm.de>
2024-10-15 21:12:38 +0200

    testing odmr logic

--- tests/test_OdmrLogic.py
+++ tests/test_OdmrLogic.py
@@ -35,29 +35,29 @@ FIT_MODEL = 'Gaussian Dip'
 TOLERANCE = 10 # tolerance for signal data range
 
 
-def get_scanner(module):
+def get_scanner(logic_instance):
     """
-    Getter for scanner module instance for logic module.
+    Getter for scanner logic_instance logic_instance for logic logic_instance.
 
     Parameters
     ----------
-    module : Object
-        logic module instance
+    logic_instance : Object
+        logic logic_instance logic_instance
 
     Returns
     -------
     Object
         Scanner module instance
     """
-    return module._data_scanner()
+    return logic_instance._data_scanner()
 
-def get_microwave(module):
+def get_microwave(logic_instance):
     """
     Getter for microwave module instance for logic module.
 
     Parameters
     ----------
-    module : Object
+    logic_instance : Object
         logic module instance
 
     Returns
@@ -65,7 +65,7 @@ def get_microwave(module):
     Object
         microwave module instance
     """
-    return module._microwave()
+    return logic_instance._microwave()
 
 def get_odmr_range(length, scanner):
     """
@@ -107,8 +107,8 @@ def get_tolerance(value, bound):
     return int(value + value * TOLERANCE/100) if bound == 'upper' else int(value - value * TOLERANCE/100)
 
 
-@pytest.fixture(scope='module')
-def module(remote_instance):
+#@pytest.fixture(scope='logic_instance')
+def get_logic_module_instance(remote_instance):
     """
     Fixture that returns ODMR logic instance.
 
@@ -145,37 +145,38 @@ def coverage_for_each_test(request):
     print(f"Coverage report saved to {test_dir}")
 
 
-def test_start_odmr_scan(module):
+def test_start_odmr_scan(remote_instance):
     """
     Tests if the scan parameters are correctly generated and if the signal data is generated for the given runtime
     with appropriate values.
 
     Parameters
     ----------
-    module : fixture
-        Fixture for instance of ODMR logic module
+    logic_instance : fixture
+        Fixture for logic_instance of ODMR logic logic_instance
     scanner : fixture
-        Fixture for connected instance of Finite sampling input dummy for data scanning
+        Fixture for connected logic_instance of Finite sampling input dummy for data scanning
     qtbot : fixture
         Fixture for qt support
     """
-    scanner = get_scanner(module)
-    module.runtime = 5
-    freq_low, freq_high, freq_counts = list(map(int, module.frequency_ranges[0]))
-    frequency_data = module.frequency_data
-    assert len(frequency_data) == module.frequency_range_count
+    logic_instance = get_logic_module_instance(remote_instance)
+    scanner = get_scanner(logic_instance)
+    logic_instance.runtime = 5
+    freq_low, freq_high, freq_counts = list(map(int, logic_instance.frequency_ranges[0]))
+    frequency_data = logic_instance.frequency_data
+    assert len(frequency_data) == logic_instance.frequency_range_count
     for data_range in frequency_data:
         assert len(data_range) == freq_counts
         for freq_value in data_range:
             assert isinstance(freq_value, float)
             assert int(freq_value) in range(freq_low, freq_high+1)
     
-    module.start_odmr_scan()
-    run_time = int(module._run_time) 
-    #with qtbot.waitSignals( [module._sigNextLine]*run_time, timeout = run_time*1500) as blockers:
+    logic_instance.start_odmr_scan()
+    run_time = int(logic_instance._run_time) 
+    #with qtbot.waitSignals( [logic_instance._sigNextLine]*run_time, timeout = run_time*1500) as blockers:
     #    pass
     time.sleep(run_time)
-    signal_data = module.signal_data
+    signal_data = logic_instance.signal_data
     assert len(signal_data) == len(scanner.active_channels)
     odmr_range = get_odmr_range(5, scanner)
     for channel in signal_data:
@@ -185,35 +186,38 @@ def test_start_odmr_scan(module):
                 assert isinstance(value,float)
                 assert not math.isnan(value)
                 assert int(value) in range(*odmr_range[channel])
-    #print(f'elspased sweeps {module._elapsed_sweeps}') 
+    #print(f'elspased sweeps {logic_instance._elapsed_sweeps}') 
 
-def test_do_fit(module):
+def test_do_fit(remote_instance):
     """
     Tests if the fitting of the generated signal data works by checking the values of the fit parameters are not nan.
 
     Parameters
     ----------
-    module : fixture
+    remote_instance : fixture
         Fixture for instance of ODMR logic module
     """
-    module.do_fit(FIT_MODEL, CHANNELS[0], 0)
-    fit_results  = module.fit_results[CHANNELS[0]][0][1]
-    dict_fit_result = module.fit_container.dict_result(fit_results)
+    logic_instance = get_logic_module_instance(remote_instance)
+    logic_instance.do_fit(FIT_MODEL, CHANNELS[0], 0)
+    fit_results  = logic_instance.fit_results[CHANNELS[0]][0][1]
+    dict_fit_result = logic_instance.fit_container.dict_result(fit_results)
     for key,values in dict_fit_result.items():
         if 'value' in values:
             assert not math.isnan(values['value'])
 
-def test_save_odmr_data(module):
+def test_save_odmr_data(remote_instance):
     """
     Tests whether new files were saved in the save dir after executing the save function. If a new file exists, the
     contents are checked against the actual signal data to ensure that the data is saved correctly.
 
     Parameters
     ----------
-    module : fixture
-        Fixture for instance of ODMR logic module
+    logic_instance : fixture
+        Fixture for logic_instance of ODMR logic logic_instance
     """
-    save_dir = module.module_default_data_dir
+    logic_instance = get_logic_module_instance(remote_instance)
+
+    save_dir = logic_instance.module_default_data_dir
     if os.path.exists(save_dir):
         saved_files = os.listdir(save_dir)
         saved_files = [os.path.join(save_dir, file) for file in saved_files]
@@ -222,7 +226,7 @@ def test_save_odmr_data(module):
         time_diffs =   current_time - creation_times
         assert(not any(time_diffs<5)) # no files should be created in the last 5 secs before saving
         
-    module.save_odmr_data()
+    logic_instance.save_odmr_data()
     
     saved_files = os.listdir(save_dir)
     saved_files = [os.path.join(save_dir, file) for file in saved_files]
@@ -235,5 +239,5 @@ def test_save_odmr_data(module):
     saved_signal_data = np.loadtxt(signal_data_file)
     for i,channel in enumerate(CHANNELS):
         saved_channel_data = [saved_signal_row[i+1]  for saved_signal_row in saved_signal_data]
-        actual_channel_data = netobtain(module.signal_data[channel][0])
+        actual_channel_data = netobtain(logic_instance.signal_data[channel][0])
         assert np.allclose(saved_channel_data, actual_channel_data)
