52869a4b60f8ab0b1c05a023a9ba9cdc417aa670
Mohit <mohit.shah@uni-ulm.de>
2025-11-29 15:15:09 +0100

    added a debugging script

---
+++ workflow_utils/debug.py
@@ -0,0 +1,14 @@
+import os
+from qudi.util.yaml import yaml_load
+
+
+def load_status_var(file_path):    
+    try:
+        variables = yaml_load(file_path, ignore_missing=True)
+        print(variables)
+    except Exception as e:
+        variables = dict()
+        print("Failed to load status variables:", e)
+
+file_path = 'saved_status_vars/status-PIDLogic_hardware_pid_logic.cfg'
+load_status_var(file_path)
